291da278794986ce135b0a05ee7b44b9a47dbda4
danjosepad <danielpadilhapessoal@outlook.com>
2020-03-17 23:49:54 -0300

    feat: UseEffect and UseState explanation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [techs, setTechs] = useState(['ReactJS', 'React Native', 'NodeJS']);
+  const [newTech, setNewTech] = useState(''); // Define o state e o seu "set"
+
+  useEffect(() => {
+    const storage = localStorage.getItem('tech');
+
+    if (storage) {
+      setTechs(JSON.parse(storage));
+    }
+  }, []); // ComponentDidMount, quando o component é montado, chama os dados
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(techs));
+  }, [techs]); // ComponentDidUpdate, quando o techs muda, chama o useEffect
+
+  function handleNewTech() {
+    setTechs([...techs, newTech]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {techs.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+      />
+      <button type="button" onClick={() => handleNewTech('Teste')}>
+        Adicionar nova tecnologia
+      </button>
+    </>
   );
 }
 
